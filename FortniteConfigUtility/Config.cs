d8a0bf9ebe64e691dd61e4c683bfdca306f5328b
Takoid <6531977+TakoidGit@users.noreply.github.com>
2018-12-10 21:12:27 -0500

    add gpl license text

--- FortniteConfigUtility/Config.cs
+++ FortniteConfigUtility/Config.cs
@@ -1,15 +1,17 @@
-﻿/* Fortnite Config Utility is free software: you can redistribute it and/or modify
+﻿/* This file is part of Fortnite Config Utility.
+
+Fortnite Config Utility is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    Fortnite Config Utility is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program. If not, see <https://www.gnu.org/licenses/>. */
+    along with Fortnite Config Utility.  If not, see<https://www.gnu.org/licenses/>. */
 
 using System;
 using System.IO;
